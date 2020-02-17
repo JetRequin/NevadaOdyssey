62b34424d2df856e538b188d23760cd961d8ec42
DylanMeta <dylanmetans83@gmail.com>
2020-02-17 12:22:07 +0100

    Add name & money in sharedpreferences

---
+++ app/src/main/java/fr/isen/nevadaodyssey/HomeActivity.kt
@@ -0,0 +1,52 @@
+package fr.isen.nevadaodyssey
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_login.*
+
+class HomeActivity : AppCompatActivity() {
+
+    var userPreferences: SharedPreferences? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        userPreferences = getSharedPreferences(UserPreferences.name, Context.MODE_PRIVATE)
+        val name = userPreferences?.getString(UserPreferences.name,null)?: ""
+        var money = userPreferences?.getInt(UserPreferences.money,0) ?: 0
+
+        textViewUserName.text = name
+        textViewUserMoney.text = "Money:"+ money.toString() +"$"
+
+        //TODO:To each image button, link corresponding activity
+
+             imageButtonBlackJack.setOnClickListener{
+                 //val intentBlackJack = Intent(this, BlackJackActivity::class.java)
+                 //startActivity(intentBlackJack)
+                 money+= 50
+                 textViewUserMoney.text = "Money:"+ money.toString() +"$"
+             }
+
+        /*
+             imageButtonPokerDice.setOnClickListener {
+                 val intentPokerDice = Intent(this, PokerDiceActivity::class.java)
+                 startActivity(intentPokerDice)
+             }
+
+             imageButtonRoulette.setOnClickListener{
+                 val intentRoulette = Intent(this, RouletteActivity::class.java)
+                 startActivity(intentRoulette)
+             }
+
+             imageButtonSlotMachine.setOnClickListener {
+                 val intentSlotMachine = Intent(this, SlotMachineActivity::class.java)
+                 startActivity(intentSlotMachine)
+             }
+              */
+    }
+}
--- app/src/main/java/fr/isen/nevadaodyssey/LoginActivity.kt
+++ app/src/main/java/fr/isen/nevadaodyssey/LoginActivity.kt
@@ -1,12 +1,50 @@
 package fr.isen.nevadaodyssey
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    var userPreferences: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        userPreferences = getSharedPreferences(UserPreferences.name, Context.MODE_PRIVATE)
+
+
+        buttonLogin.setOnClickListener{
+
+            saveUser(editTextLogin.text.toString())
+            checkUserValues()
+
+            val intentWallActivity = Intent(this, HomeActivity::class.java)
+            intentWallActivity.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intentWallActivity)
+            finish()
+        }
+    }
+
+    private fun saveUser(name: String) {
+        val editor = userPreferences?.edit()
+        editor?.putString(UserPreferences.name, name)
+        editor?.putInt(UserPreferences.money,10000)
+        editor?.apply()
+    }
+
+     fun checkUserValues(){
+        val name = userPreferences?.getString(UserPreferences.name,null)?: ""
+
+        Toast.makeText(
+            this,
+            "Coucou $name",
+            Toast.LENGTH_LONG).show()
+
     }
 }
