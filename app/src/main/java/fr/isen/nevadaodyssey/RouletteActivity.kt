9ae339003a6e6963ce185334c2debd9a9c35db2e
Thibaut C <t.chevallier11@gmail.com>
2020-03-11 09:38:17 +0100

    Dernier commit : pari sur le numéro

--- app/src/main/java/fr/isen/nevadaodyssey/RouletteActivity.kt
+++ app/src/main/java/fr/isen/nevadaodyssey/RouletteActivity.kt
@@ -77,29 +77,39 @@ class RouletteActivity : AppCompatActivity() {
                     override fun onAnimationEnd(animation: Animation?) {
                         val currentNum = currentNumber(360 - (degree % 360))[0]
                         val myColor = currentNumber(360 - (degree % 360))[1]
+                        val myNumber = currentNumber(360 - (degree % 360))[2]
                         result.text = currentNum
-
-                        if(myColor == "red" && switch1.isChecked){
-                            money = (money + (mise.text.toString().toInt()*1.5)).toInt()
-                            moneyAmount.text = money.toString()                        }
-                        else {
-                            if (myColor == "black" && switch1.isChecked) {
-                                money -= mise.text.toString().toInt()
+                        if(numberBet.text.isNotEmpty()){
+                            if(numberBet.text.toString() == myNumber){
+                                money = (money + (mise.text.toString().toInt() * 10))
                                 moneyAmount.text = money.toString()
                             }
                             else{
-                                if (myColor == "red") {
-                                    money -= mise.text.toString().toInt()
-                                    moneyAmount.text = money.toString()
-                                }
-                                else{
-                                    money = (money + (mise.text.toString().toInt()*1.5)).toInt()
-                                    moneyAmount.text = money.toString()
-                                }
+                                money -= mise.text.toString().toInt()
+                                moneyAmount.text = money.toString()
                             }
                         }
+                        else {
+                            if (myColor == "red" && switch1.isChecked) {
+                                money = (money + (mise.text.toString().toInt() * 1.5)).toInt()
+                                moneyAmount.text = money.toString()
+                            } else {
+                                if (myColor == "black" && switch1.isChecked) {
+                                    money -= mise.text.toString().toInt()
+                                    moneyAmount.text = money.toString()
+                                } else {
+                                    if (myColor == "red") {
+                                        money -= mise.text.toString().toInt()
+                                        moneyAmount.text = money.toString()
+                                    } else {
+                                        money =
+                                            (money + (mise.text.toString().toInt() * 1.5)).toInt()
+                                        moneyAmount.text = money.toString()
+                                    }
+                                }
+                            }
 
-
+                        }
 
                     }
 
@@ -137,180 +147,216 @@ class RouletteActivity : AppCompatActivity() {
         if(degrees >= (FACTOR*1) && degrees < (FACTOR*3)){
             text = "32 red"
             color = "red"
+            number = "32"
         }
         if(degrees >= (FACTOR*3) && degrees < (FACTOR*5)){
             text = "15 black"
             color = "black"
+            number = "15"
 
         }
         if(degrees >= (FACTOR*5) && degrees < (FACTOR*7)){
             text = "19 red"
             color = "red"
+            number = "19"
 
         }
         if(degrees >= (FACTOR*7) && degrees < (FACTOR*9)){
             text = "4 black"
             color = "black"
+            number = "4"
 
         }
         if(degrees >= (FACTOR*9) && degrees < (FACTOR*11)){
             text = "21 red"
             color = "red"
+            number = "21"
 
         }
         if(degrees >= (FACTOR*11) && degrees < (FACTOR*13)){
             text = "2 black"
             color = "black"
+            number = "2"
 
         }
         if(degrees >= (FACTOR*13) && degrees < (FACTOR*15)){
             text = "25 red"
             color = "red"
+            number = "25"
 
         }
         if(degrees >= (FACTOR*15) && degrees < (FACTOR*17)){
             text = "17 black"
             color = "black"
+            number = "17"
 
         }
         if(degrees >= (FACTOR*17) && degrees < (FACTOR*19)){
             text = "34 red"
             color = "red"
+            number = "34"
 
         }
         if(degrees >= (FACTOR*19) && degrees < (FACTOR*21)){
             text = "6 black"
             color = "black"
+            number = "6"
 
         }
         if(degrees >= (FACTOR*21) && degrees < (FACTOR*23)){
             text = "27 red"
             color = "red"
+            number = "27"
 
         }
         if(degrees >= (FACTOR*23) && degrees < (FACTOR*25)){
             text = "13 black"
             color = "black"
+            number = "13"
 
         }
         if(degrees >= (FACTOR*25) && degrees < (FACTOR*27)){
             text = "36 red"
             color = "red"
+            number = "36"
 
         }
         if(degrees >= (FACTOR*27) && degrees < (FACTOR*29)){
             text = "11 black"
             color = "black"
+            number = "11"
 
         }
         if(degrees >= (FACTOR*29) && degrees < (FACTOR*31)){
             text = "30 red"
             color = "red"
+            number = "30"
 
         }
         if(degrees >= (FACTOR*31) && degrees < (FACTOR*33)){
             text = "8 black"
             color = "black"
+            number = "8"
 
         }
         if(degrees >= (FACTOR*33) && degrees < (FACTOR*35)){
             text = "23 red"
             color = "red"
+            number = "23"
 
         }
         if(degrees >= (FACTOR*35) && degrees < (FACTOR*37)){
             text = "10 black"
             color = "black"
+            number = "10"
 
         }
         if(degrees >= (FACTOR*37) && degrees < (FACTOR*39)){
             text = "5 red"
             color = "red"
+            number = "5"
 
         }
         if(degrees >= (FACTOR*39) && degrees < (FACTOR*41)){
             text = "24 black"
             color = "black"
+            number = "24"
 
         }
         if(degrees >= (FACTOR*41) && degrees < (FACTOR*43)){
             text = "16 red"
             color = "red"
+            number = "16"
 
         }
         if(degrees >= (FACTOR*43) && degrees < (FACTOR*45)){
             text = "33 black"
             color = "black"
+            number = "33"
 
         }
         if(degrees >= (FACTOR*45) && degrees < (FACTOR*47)){
             text = "1 red"
             color = "red"
+            number = "1"
 
         }
         if(degrees >= (FACTOR*47) && degrees < (FACTOR*49)){
             text = "20 black"
             color = "black"
+            number = "20"
 
         }
         if(degrees >= (FACTOR*49) && degrees < (FACTOR*51)){
             text = "14 red"
             color = "red"
+            number = "14"
 
         }
         if(degrees >= (FACTOR*51) && degrees < (FACTOR*53)){
             text = "31 black"
             color = "black"
+            number = "31"
 
         }
         if(degrees >= (FACTOR*53) && degrees < (FACTOR*55)){
             text = "9 red"
             color = "red"
+            number = "9"
 
         }
         if(degrees >= (FACTOR*55) && degrees < (FACTOR*57)){
             text = "22 black"
             color = "black"
+            number = "22"
 
         }
         if(degrees >= (FACTOR*57) && degrees < (FACTOR*59)){
             text = "18 red"
             color = "red"
+            number = "18"
 
         }
         if(degrees >= (FACTOR*59) && degrees < (FACTOR*61)){
             text = "29 black"
             color = "black"
+            number = "29"
 
         }
         if(degrees >= (FACTOR*61) && degrees < (FACTOR*63)){
             text = "7 red"
             color = "red"
+            number = "7"
 
         }
         if(degrees >= (FACTOR*63) && degrees < (FACTOR*65)){
             text = "28 black"
             color = "black"
+            number = "28"
 
         }
         if(degrees >= (FACTOR*65) && degrees < (FACTOR*67)){
             text = "12 red"
             color = "red"
+            number = "12"
 
         }
         if(degrees >= (FACTOR*67) && degrees < (FACTOR*69)){
             text = "35 black"
             color = "black"
+            number = "35"
 
         }
         if(degrees >= (FACTOR*69) && degrees < (FACTOR*71)){
             text = "3 red"
             color = "red"
+            number = "3"
 
         }
         if(degrees >= (FACTOR*71) && degrees < (FACTOR*73)){
             text = "26 black"
             color = "black"
+            number = "26"
 
         }
         if((degrees >= (FACTOR*73) && degrees < 360) || (degrees >= 0 && degrees < (FACTOR*1))){
@@ -319,7 +365,7 @@ class RouletteActivity : AppCompatActivity() {
             number="0"
 
         }
-        val tab = arrayOf(text, color)
+        val tab = arrayOf(text, color, number)
         return tab
     }
 }
